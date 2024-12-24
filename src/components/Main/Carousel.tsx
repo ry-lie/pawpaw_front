1e471b3572e3343d5585100481451a920ba369c9
[React1] 이서빈 <tjqls0916@naver.com>
2024-12-24 14:56:58 +0900

    fix/carousel디테일 수정

--- src/components/Main/Carousel.tsx
+++ src/components/Main/Carousel.tsx
@@ -2,12 +2,17 @@
 
 import { useState } from "react";
 import Image from "next/image";
+import PrevArrow from "@/assets/icons/prevArrow.png"
+import NextArrow from "@/assets/icons/nextArrow.png"
 import Carousel1 from "@/assets/images/carousel/carousel1.png";
 import Carousel2 from "@/assets/images/carousel/carousel2.png";
+import Carousel3 from "@/assets/images/carousel/carousel3.png";
+ 
 
 const carouselData = [
-  { id: 1, imgUrl: Carousel1, text: "Slide 1" },
-  { id: 2, imgUrl: Carousel2, text: "Slide 2" },
+  { id: 1, imgUrl: Carousel1, text: "반려동물과\n함께하는 일상" },
+  { id: 2, imgUrl: Carousel2, text: "반려동물과\n어디든지 함께해요" },
+  { id: 3, imgUrl: Carousel3, text: "포포에서 만나는\n산책메이트" },
 ];
 
 const Carousel = () => {
@@ -39,13 +44,13 @@ const Carousel = () => {
           >
             <Image
               src={item.imgUrl}
-              alt={item.text}
+              alt="Carousel Image"
               fill
               className="object-cover"
             />
-            <p className="absolute bottom-4 text-white text-2xl font-bold bg-black/50 p-2 rounded">
-              {item.text}
-            </p>
+            <div className="absolute inset-0 flex top-[20%] left-[10%] text-black text-3xl font-medium whitespace-pre-line">
+              {item.text}     
+            </div>
           </div>
         ))}
       </div>
@@ -53,17 +58,25 @@ const Carousel = () => {
       {/* Left Arrow */}
       <button
         onClick={handlePrev}
-        className="absolute top-1/2 left-4 transform -translate-y-1/2 bg-black/50 text-white p-2 rounded-full hover:bg-black"
+        className="absolute top-1/2 left-4 transform -translate-y-1/2 p-0.5 rounded-full hover:bg-white hover:bg-opacity-50 transition duration-200"
       >
-        &#8592;
+        <Image
+          src={PrevArrow}
+          alt="prev icon"
+          className="w-10 h-10"
+        />
       </button>
 
       {/* Right Arrow */}
       <button
         onClick={handleNext}
-        className="absolute top-1/2 right-4 transform -translate-y-1/2 bg-black/50 text-white p-2 rounded-full hover:bg-black"
+        className="absolute top-1/2 right-4 transform -translate-y-1/2 p-0.5 rounded-full hover:bg-white hover:bg-opacity-50 transition duration-200"
       >
-        &#8594;
+        <Image
+          src={NextArrow}
+          alt="prev icon"
+          className="w-10 h-10"
+        />
       </button>
 
       {/* Indicators */}
@@ -71,7 +84,7 @@ const Carousel = () => {
         {carouselData.map((_, index) => (
           <div
             key={index}
-            className={`w-3 h-3 rounded-full ${currentIndex === index ? "bg-white" : "bg-gray-400"
+            className={`w-2 h-2 rounded-full ${currentIndex === index ? "bg-white" : "bg-gray-400"
               }`}
           />
         ))}
