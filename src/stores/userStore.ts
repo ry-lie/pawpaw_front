f5036ac32685f377d67fd117202cea6ed3650839
[React1] 이주영 <gmltns5723@kakao.com>
2024-12-28 02:50:05 +0900

    ✨ 리버스프록시 설정, 로그인 사용자 스토어 작성
    리버스프록시 설정, 로그인 사용자 스토어 작성

---
+++ src/stores/userStore.ts
@@ -0,0 +1,43 @@
+"use client";
+
+import { create } from "zustand";
+
+interface UserState {
+  id: number;
+  nickname: string;
+  isLoggedIn: boolean;
+  login: (user: { id: number; nickname: string }) => void; // 로그인
+  logout: () => void; // 로그아웃
+  initialize: () => void; // 초기화
+}
+
+export const useUserStore = create<UserState>((set) => ({
+  id: 0,
+  nickname: "",
+  isLoggedIn: false,
+
+  // 로그인
+  login: (user) =>
+    set(() => {
+      console.log("로그인 호출됨:", user);
+      const sessionData = { ...user, isLoggedIn: true };
+      sessionStorage.setItem("user", JSON.stringify(sessionData));
+      return sessionData;
+    }),
+
+  // 로그아웃
+  logout: () =>
+    set(() => {
+      sessionStorage.removeItem("user");
+      return { id: 0, nickname: "", isLoggedIn: false };
+    }),
+  // 세션 스토리지 초기화
+  initialize: () =>
+    set(() => {
+      const storedUser = sessionStorage.getItem("user");
+      if (storedUser) {
+        return JSON.parse(storedUser);
+      }
+      return { id: 0, nickname: "", isLoggedIn: false };
+    }),
+}));
