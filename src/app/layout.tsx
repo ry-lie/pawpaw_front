b77290c38bbb0daf886e0e565a4c0e8840da4835
jay <hjh268100@gmail.com>
2024-12-30 14:42:07 +0900

    🐛 Fix : 임시파일

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,10 @@ import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 
 export const metadata: Metadata = {
-  title: "포포(pawpaw) - 네 발 달린 친구들(4paws)과 행복한 추억 만들기를 위한 커뮤니티",
-  description: "다양한 카테고리별 커뮤니티, 위치기반 반려 동물 동반 장소 추천, 내 주변 산책메이트 찾기, 실시간 채팅",
+  title:
+    "포포(pawpaw) - 네 발 달린 친구들(4paws)과 행복한 추억 만들기를 위한 커뮤니티",
+  description:
+    "다양한 카테고리별 커뮤니티, 위치기반 반려 동물 동반 장소 추천, 내 주변 산책메이트 찾기, 실시간 채팅",
 };
 
 export default function RootLayout({
@@ -31,7 +33,11 @@ export default function RootLayout({
             <div className="flex flex-col w-full min-h-screen sm:max-w-mobile mx-auto bg-background border border-stroke_gray-600">
               {children}
               <Modal />
-              <ToastContainer position="top-right" autoClose={2000}/>
+              <ToastContainer
+                position="top-right"
+                autoClose={2000}
+                className="text-sm pt-12 pr-3 mb-1"
+              />
             </div>
           </Suspense>
         </QueryProvider>
--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -16,7 +16,6 @@ import Image from "next/image";
 const NONE_NAV_PAGE_LIST = [PATHS.LOGIN, PATHS.COMMUNITY_WRITE] as string[];
 
 export default function Nav() {
-
   const router = useRouter();
   const alarms = useAlarmStore((state) => state.alarms);
   const addAlarm = useAlarmStore((state) => state.addAlarm);
@@ -37,19 +36,22 @@ export default function Nav() {
   //알람이 없음
   const handleNoMessage = () => {
     if (alarms.length === 0) {
-      toast.info("새로운 알람이 없습니다.", {
+      toast("새로운 알람이 없습니다.", {
         position: "top-right",
         autoClose: false,
         closeOnClick: true,
         draggable: true,
         hideProgressBar: true,
       });
-    };
+    }
   };
 
   //다른사람이 나에게 알람을 요청
   const handleRequestChat = (sender: string) => {
-    const message = `${sender}님이 채팅 요청을 보냈습니다.`
+    if (!isLoggedIn) {
+      return;
+    }
+    const message = `${sender}님의 대화요청`;
     addAlarm({ sender, message, status: "pending" });
     const toastId = toast(
       <div>
@@ -57,11 +59,14 @@ export default function Nav() {
         <div>
           <Button
             onClick={() => handleApprove(sender, toastId)}
-            containerStyles="!text-base !bg-white !text-blue-500 !font-normal hover:!underline hover:!underline-offset-4 mr-1">
+            containerStyles="!text-base !bg-white !text-blue-500 !font-normal hover:!underline hover:!underline-offset-4 mr-1"
+          >
             수락
           </Button>
-          <Button onClick={() => handleReject(sender, toastId)}
-            containerStyles="!text-base !bg-white !text-red-500 !font-normal hover:!underline hover:!underline-offset-4 mr-1">
+          <Button
+            onClick={() => handleReject(sender, toastId)}
+            containerStyles="!text-base !bg-white !text-red-500 !font-normal hover:!underline hover:!underline-offset-4 mr-1"
+          >
             거절
           </Button>
         </div>
@@ -73,42 +78,44 @@ export default function Nav() {
         closeOnClick: false,
         draggable: false,
         hideProgressBar: true,
-      }
+      },
     );
   };
 
   //채팅 요청수락
   const handleApprove = (sender: string, toastId: React.ReactText) => {
-    toast.dismiss(toastId)
-    toast.info(`${sender}님의 채팅을 수락했습니다.`, {
+    toast.dismiss(toastId);
+    toast.success(`${sender}님의 채팅을 수락했습니다.`, {
       autoClose: 800,
-      position: "top-right"
+      position: "top-right",
     });
     updateAlarmStatus(sender, "accepted");
     const { nickname } = useUserStore.getState();
     if (nickname) {
       router.push(`/chat?sender=${nickname}&receiver=${sender}`);
     } else {
-      toast.error("로그인이 필요합니다.", { autoClose: 1000 })
+      toast.error("로그인이 필요합니다.", { autoClose: 1000 });
     }
-
-  }
+  };
 
   //채팅 요청거절
   const handleReject = (sender: string, toastId: React.ReactText) => {
-    toast.dismiss(toastId)
-    toast.info(`${sender}님의 채팅을 거절했습니다.`, {
+    toast.dismiss(toastId);
+    toast.error(`${sender}님의 채팅을 거절했습니다.`, {
       autoClose: 800,
-      position: "top-right"
+      position: "top-right",
     });
     removeAlarm(sender);
-  }
+  };
 
   //기존의 채팅방에서 새로운 메세지가 도착
   const ApproveChatNewMessage = (alarm: AlarmProps) => {
     if (alarm.status === "accepted") {
       toast(
-        <div onClick={() => moveChatRoom(alarm.sender)} className="cursor-pointer">
+        <div
+          onClick={() => moveChatRoom(alarm.sender)}
+          className="cursor-pointer"
+        >
           <p>{alarm.sender}</p>
           <p>{alarm.message}</p>
         </div>,
@@ -118,10 +125,10 @@ export default function Nav() {
           closeOnClick: false,
           draggable: false,
           hideProgressBar: true,
-        }
-      )
+        },
+      );
     }
-  }
+  };
 
   //채팅방 이동
   const moveChatRoom = (sender: string) => {
@@ -129,7 +136,7 @@ export default function Nav() {
     if (nickname) {
       router.push(`/chat?sender=${nickname}&receiver=${sender}`);
     }
-  }
+  };
 
   // 로그아웃
   const handleLogout = () => {
@@ -152,7 +159,10 @@ export default function Nav() {
       {/* 로고 영역 */}
       <div className="flex justify-between items-center h-full">
         <div className="flex items-center">
-          <Link href={PATHS.MAIN} className="text-lg font-semibold text-gray-800">
+          <Link
+            href={PATHS.MAIN}
+            className="text-lg font-semibold text-gray-800"
+          >
             LOGO
           </Link>
         </div>
@@ -169,45 +179,45 @@ export default function Nav() {
               </button>
               {/*알람 버튼 진행 */}
               <Button
-              onClick={()=>{
-                if(alarms.length===0){
-                  handleNoMessage();
-                } else{
-                  alarms.forEach((alarm)=>{
-                    if(alarm.status ==="pending"){
-                      handleRequestChat(alarm.sender);
-                    }else if(alarm.status ==="accepted"){
-                      ApproveChatNewMessage(alarm);
-                    }
-                  });
-                  useAlarmStore.getState().setAlarms([]);
-                }
-              }}
-              containerStyles="h-6 w-6 !bg-transparent"
+                onClick={() => {
+                  if (alarms.length === 0) {
+                    handleNoMessage();
+                  } else {
+                    alarms.forEach((alarm) => {
+                      if (alarm.status === "pending") {
+                        handleRequestChat(alarm.sender);
+                      } else if (alarm.status === "accepted") {
+                        ApproveChatNewMessage(alarm);
+                      }
+                    });
+                    useAlarmStore.getState().setAlarms([]);
+                  }
+                }}
+                containerStyles="h-6 w-6 !bg-transparent"
               >
-                <Image src={alarmsIcon} alt="알람아이콘" />
+                <Image src={alarmsIcon} alt="알람아이콘" width={6} height={6} />
               </Button>
-
             </>
           ) : (
             <>
-              <Link href={PATHS.LOGIN} className="text-sm text-gray-600 hover:text-gray-800 transition">
+              <Link
+                href={PATHS.LOGIN}
+                className="text-sm text-gray-600 hover:text-gray-800 transition"
+              >
                 로그인
               </Link>
-              <Link href={PATHS.JOIN} className="text-sm text-gray-600 hover:text-gray-800 transition">
+              <Link
+                href={PATHS.JOIN}
+                className="text-sm text-gray-600 hover:text-gray-800 transition"
+              >
                 회원가입
               </Link>
             </>
-          )
-          }
+          )}
         </div>
-
-
-
       </div>
     </nav>
   );
 }
-
 
 //알람이 자동적으로 뜨게 하는건 socket으로 진행?
