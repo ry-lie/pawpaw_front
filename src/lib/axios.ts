8db6598dce51498fd022dfd2cb18daa78f399299
jay <hjh268100@gmail.com>
2024-12-31 14:08:41 +0900

    ✨ Feat : Toast정리 및 axios오류 작성
    공용 Toast와 catch문에서 toast에서 작성되어야 할 내용을 정리하여 추후 console.error(e)로만 조치하게 진행함

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,8 @@
-import axios from "axios";
+import axios, { AxiosError, isAxiosError } from "axios";
 import { getSession } from "next-auth/react";
 import { redirect } from "next/navigation";
 import { PATHS } from "../constants/path";
+import { errorToast } from "@/utils/Toast";
 
 const axiosInstance = axios.create({
   baseURL: "/api",
@@ -16,13 +17,41 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
-      console.error("권한없음");
-      redirect(PATHS.LOGIN);
+    if (isAxiosError(error)) {
+      if (!isServerApiError(error)) {
+        errorToast("서버와 통신이 원활하지 않습니다.");
+      }
+
+      if (error.status === 401) {
+        errorToast("로그인이 필요합니다.");
+        redirect(PATHS.LOGIN);
+      }
+      if (isServerApiError(error)) {
+        errorToast(error.response.data.message);
+      }
+      return Promise.reject(error);
     }
+    errorToast(error.message);
     console.error("응답 인터셉터 에러:", error);
-    return Promise.reject(error);
   },
 );
 
 export default axiosInstance;
+
+interface ErrorMeta {
+  response: {
+    data: {
+      status: number | string;
+      message: string;
+    };
+  };
+  status: number;
+}
+
+const isServerApiError = (error: any): error is ErrorMeta => {
+  return isAxiosError(error) && !!error.response?.data?.message;
+  //전달될 에러가 axios 라이브러리를 통해 전달된것인가?
+
+  //참일때, 전달된 인자를 ErrorMeta라는 타입으로 간주
+  //axiox를 통해 호출된 에러 메세지 존해한다면?
+};
