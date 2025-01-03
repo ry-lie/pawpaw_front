b054053400045cf3d8b5205f7c24fe18b5b807de
jay <hjh268100@gmail.com>
2025-01-03 11:12:13 +0900

    ✏️ Fix : nav의 SSE작업진행

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,134 +5,20 @@ import { useUserStore } from "@/stores/userStore";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
-import Button from "../Button";
-import { toast } from "react-toastify";
 import { useAlarmStore } from "@/stores/alarmStore";
-import { AlarmProps } from "@/stores/alarmStore";
 import Alrem_off from "@/assets/icons/alrem_off.png";
 import Alrem_on from "@/assets/icons/alrem_on.png";
 import Image from "next/image";
 import MiniLogo from "@/assets/images/logo/miniLogo.png";
+import ChatEventAlram from "./ChatEventAlarm";
 
 const NONE_NAV_PAGE_LIST = [PATHS.LOGIN, PATHS.COMMUNITY_WRITE] as string[];
 
 export default function Nav() {
   const router = useRouter();
   const alarms = useAlarmStore((state) => state.alarms);
-  const addAlarm = useAlarmStore((state) => state.addAlarm);
-  const updateAlarmStatus = useAlarmStore((state) => state.updateAlarmStatus);
-  const removeAlarm = useAlarmStore((state) => state.removeAlarm);
   const { isLoggedIn, logout } = useUserStore(); // Zustand에서 상태 가져오기
   const [isLoading, setIsLoading] = useState(false);
-
-  //알람이 없음
-  const handleNoMessage = () => {
-    if (alarms.length === 0) {
-      toast("새로운 알람이 없습니다.", {
-        position: "top-right",
-        autoClose: false,
-        closeOnClick: true,
-        draggable: true,
-        hideProgressBar: true,
-      });
-    }
-  };
-
-  //다른사람이 나에게 알람을 요청
-  const handleRequestChat = (sender: string) => {
-    if (!isLoggedIn) {
-      return;
-    }
-    const message = `${sender}님의 대화요청`;
-    addAlarm({ sender, message, status: "pending" });
-    const toastId = toast(
-      <div>
-        <p>{message}</p>
-        <div>
-          <Button
-            disabled={isLoading}
-            onClick={() => handleApprove(sender, toastId)}
-            containerStyles="!text-base !bg-white !text-blue-500 !font-normal hover:!underline hover:!underline-offset-4 mr-1"
-          >
-            수락
-          </Button>
-          <Button
-            disabled={isLoading}
-            onClick={() => handleReject(sender, toastId)}
-            containerStyles="!text-base !bg-white !text-red-500 !font-normal hover:!underline hover:!underline-offset-4 mr-1"
-          >
-            거절
-          </Button>
-        </div>
-      </div>,
-
-      {
-        position: "top-right",
-        autoClose: false,
-        closeOnClick: false,
-        draggable: false,
-        hideProgressBar: true,
-      },
-    );
-  };
-
-  //채팅 요청수락
-  const handleApprove = (sender: string, toastId: React.ReactText) => {
-    setIsLoading(true);
-    toast.dismiss(toastId);
-    toast.success(`${sender}님의 채팅을 수락했습니다.`, {
-      autoClose: 800,
-      position: "top-right",
-    });
-    updateAlarmStatus(sender, "accepted");
-    const { nickname } = useUserStore.getState();
-    if (nickname) {
-      router.push(`/chat?sender=${nickname}&receiver=${sender}`);
-    } else {
-      toast.error("로그인이 필요합니다.", { autoClose: 1000 });
-    }
-  };
-
-  //채팅 요청거절
-  const handleReject = (sender: string, toastId: React.ReactText) => {
-    setIsLoading(true);
-    toast.dismiss(toastId);
-    toast.error(`${sender}님의 채팅을 거절했습니다.`, {
-      autoClose: 800,
-      position: "top-right",
-    });
-    removeAlarm(sender);
-  };
-
-  //기존의 채팅방에서 새로운 메세지가 도착
-  const ApproveChatNewMessage = (alarm: AlarmProps) => {
-    if (alarm.status === "accepted") {
-      toast(
-        <div
-          onClick={() => moveChatRoom(alarm.sender)}
-          className="cursor-pointer"
-        >
-          <p>{alarm.sender}</p>
-          <p>{alarm.message}</p>
-        </div>,
-        {
-          position: "top-right",
-          autoClose: false,
-          closeOnClick: false,
-          draggable: false,
-          hideProgressBar: true,
-        },
-      );
-    }
-  };
-
-  //채팅방 이동
-  const moveChatRoom = (sender: string) => {
-    const { nickname } = useUserStore.getState();
-    if (nickname) {
-      router.push(`/chat?sender=${nickname}&receiver=${sender}`);
-    }
-  };
 
   // 로그아웃
   const handleLogout = () => {
@@ -176,26 +62,7 @@ export default function Nav() {
               >
                 로그아웃
               </button>
-              {/*알람 버튼 진행 */}
-              <Button
-                onClick={() => {
-                  if (alarms.length === 0) {
-                    handleNoMessage();
-                  } else {
-                    alarms.forEach((alarm) => {
-                      if (alarm.status === "pending") {
-                        handleRequestChat(alarm.sender);
-                      } else if (alarm.status === "accepted") {
-                        ApproveChatNewMessage(alarm);
-                      }
-                    });
-                    useAlarmStore.getState().setAlarms([]);
-                  }
-                }}
-                containerStyles="h-6 w-6 !bg-transparent"
-              >
-                <Image src={alarmsIcon} alt="알람아이콘" />
-              </Button>
+              <ChatEventAlram />
             </>
           ) : (
             <>
