85f625cc97363ccf29d8652fc3ec94d26774c0e3
jay <hjh268100@gmail.com>
2025-01-05 22:35:23 +0900

    🐛 Fix : 비밀번호 찾기 코드 수정 및 로그인 수정
    비밀번호 찾기를 해서 임시 비밀번호를 받는데 이메일에서 받은 임시 비밀번호가 -가 있을때 로그인이 안됨. 로그인시 비밀번호 정규식약간 수정

--- src/app/auth/find-password/FindPasswordForm.tsx
+++ src/app/auth/find-password/FindPasswordForm.tsx
@@ -2,103 +2,85 @@
 
 import Button from "@/components/Button";
 import Input from "@/components/Input";
-import { useForm, useWatch } from "react-hook-form";
-import { findPasswordApis } from "./useFindPassword";
-import { errorToast, infoToast, successToast } from "@/utils/Toast";
+import { useForm } from "react-hook-form";
+import { errorToast, successToast } from "@/utils/Toast";
 import { useState } from "react";
+import { useDuplicateCheck } from "../join/useDuplicateCheck";
+import { temporaryPassword } from "@/lib/api/auth";
+import { useRouter } from "next/navigation";
+import { PATHS } from "@/constants/path";
+import { handleEmailVerificationUseFindPassword } from "./useFindPassword";
 
 type FindPasswordInput = {
   email: string;
-  code: string;
+  emailCode: string;
 };
 
 export default function FindPasswordForm() {
-  const [isEmailCheck, setIsEmailCheck] = useState(false);
-  const [isCode, setIsCode] = useState(false);
+  const {
+    register,
+    handleSubmit,
+    getValues,
+    formState: { errors, isSubmitting },
+    setError,
+  } = useForm<FindPasswordInput>({ mode: "onChange" });
+  const router = useRouter();
+
+  const [isEmailSent, setIsEmailSent] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
 
-  const {
-    handleSubmit: handleSubmitRequest,
-    register: registerRequest,
-    control: controlRequest,
-    formState: { errors: errorsRequest },
-  } = useForm<FindPasswordInput>({
-    mode: "onChange",
+  const { handleCodeVerification } = useDuplicateCheck({
+    getValues,
+    setError,
   });
 
-  const {
-    handleSubmit: handleSubmitCode,
-    register: registerCode,
-    control: controlCode,
-    formState: { errors: errorsCode },
-  } = useForm<FindPasswordInput>({
-    mode: "onChange",
-  });
-
-  const email = useWatch({
-    control: controlRequest,
-    name: "email",
-  });
-
-  const code = useWatch({
-    control: controlCode,
-    name: "code",
-  });
-
-  const isValidEmail = !email || !!errorsRequest.email;
-  const isValidCode = !code || !!errorsCode.code;
-
-  const { sendCode, verifyCodeCheck, temporaryPasswordSubmit } =
-  findPasswordApis();
-
-  const onSubmitOfRequest = async (data: FindPasswordInput) => {
+  const handleEmailSubmit = async () => {
+    const email = getValues("email");
+    if (!email) {
+      setError("email", { type: "manual", message: "이메일을 입력해주세요" });
+      return;
+    }
     try {
-      if (!data.code) {
-        await sendCode(data.email);
-        infoToast("인증번호가 이메일로 전송되었습니다.");
-        setIsEmailCheck(true);
-      }
+      setIsLoading(true);
+      await handleEmailVerificationUseFindPassword(email);
+      setIsEmailSent(true);
+      successToast("인증코드가 이메일로 발송되었습니다.");
     } catch (e) {
-      setIsEmailCheck(false);
-      errorToast(
-        e instanceof Error ? e.message : "서버에 문제가 발생하였습니다.",
-      );
+      errorToast("인증코드요청에 실패했습니다. 다시 시도해주세요.");
     } finally {
       setIsLoading(false);
     }
   };
 
-  const onSubmitofCode = async (data: FindPasswordInput) => {
+  const handleCodeSubmit = async () => {
+    const emailCode = getValues("emailCode");
+    if (!emailCode) {
+      setError("emailCode", {
+        type: "manual",
+        message: "인증코드를 입력해주세요",
+      });
+      return;
+    }
     try {
-      const result = await verifyCodeCheck(data.email, data.code);
-      if (result.success) {
-        successToast("인증번호가 확인되었습니다.");
-        setIsCode(true);
-      }
+      setIsLoading(true);
+      await handleCodeVerification();
+      successToast("인증이 완료되었습니다.");
     } catch (e) {
-      setIsCode(false);
-      errorToast(
-        e instanceof Error ? e.message : "서버에 문제가 발생하였습니다.",
-      );
+      errorToast("인증에 실패하였습니다.");
     } finally {
       setIsLoading(false);
     }
   };
 
-  const onSubmitCheck = async () => {
+  const onSubmit = async () => {
+    const email = getValues("email");
     try {
-      if (!isEmailCheck || !isCode) {
-        errorToast("이메일 또는 인증코드를 확인해주세요");
-        return;
-      }
-      await temporaryPasswordSubmit(email);
-      successToast("비밀번호가 성공적으로 변경되었습니다.");
-      setIsEmailCheck(false);
-      setIsCode(false);
+      setIsLoading(true);
+      await temporaryPassword(email);
+      successToast("임시 비밀번호가 이메일로 발송되었습니다.");
+      router.push(PATHS.LOGIN);
     } catch (e) {
-      errorToast(
-        e instanceof Error ? e.message : "서버에 문제가 발생하였습니다.",
-      );
+      errorToast("임시 비밀번호 발급에 실패하였습니다. 다시 시도해주세요.");
     } finally {
       setIsLoading(false);
     }
@@ -106,58 +88,79 @@ export default function FindPasswordForm() {
 
   return (
     <div>
-      <form onSubmit={handleSubmitRequest(onSubmitOfRequest)}>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleEmailSubmit();
+        }}
+      >
         <Input
           label="이메일"
           type="email"
           placeholder="abc@abc.com"
           className="w-80 mt-2"
-          {...registerRequest("email", {
+          {...register("email", {
             required: "이메일은 필수 입력 항목입니다.",
             pattern: {
               value: /\S+@\S+\.\S+/,
               message: "유효한 이메일 주소를 입력하세요",
             },
           })}
-          errorMessage={errorsRequest.email?.message}
+          errorMessage={errors.email?.message}
         >
-          <Button btnType="submit" containerStyles="mt-2" disabled={isValidEmail}>
+          <Button
+            btnType="submit"
+            containerStyles="mt-2 p-1"
+            disabled={!getValues("email")}
+          >
             요청
           </Button>
         </Input>
       </form>
 
-      <form onSubmit={handleSubmitCode(onSubmitofCode)}>
-        <Input
-          label="인증코드"
-          type="password"
-          placeholder="인증번호를 입력하세요"
-          className="w-80 mt-2"
-          {...registerCode("code", {
-            required: "인증번호를 입력하세요",
-            pattern: {
-              value: /^\d{5}$/,
-              message: "올바른 숫자를 입력해주세요",
-            },
-          })}
-          errorMessage={errorsCode.code?.message}
-          disabled={!isEmailCheck || isLoading}
+      {isEmailSent && (
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleCodeSubmit();
+          }}
         >
-          <Button btnType="submit" disabled={isValidCode}>
-            확인
-          </Button>
-        </Input>
-      </form>
+          <Input
+            label="인증코드"
+            type="password"
+            placeholder="인증번호를 입력하세요"
+            className="w-80 mt-2"
+            {...register("emailCode", {
+              required: "인증번호를 입력하세요",
+              pattern: {
+                value: /^\d{5}$/,
+                message: "올바른 숫자를 입력해주세요",
+              },
+            })}
+            errorMessage={errors.emailCode?.message}
+          >
+            <Button
+              btnType="submit"
+              disabled={!getValues("emailCode")}
+              containerStyles="mt-2 p-1"
+            >
+              확인
+            </Button>
+          </Input>
+        </form>
+      )}
 
-      <form onSubmit={onSubmitCheck}>
-        <Button
-          btnType="submit"
-          disabled={!isValidEmail || !isValidCode || isLoading}
-          containerStyles="h-10 w-80 mt-10"
-        >
-          전송
-        </Button>
-      </form>
+      {isEmailSent && (
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Button
+            btnType="submit"
+            disabled={isLoading}
+            containerStyles="h-10 w-80 mt-10"
+          >
+            전송
+          </Button>
+        </form>
+      )}
     </div>
   );
 }
