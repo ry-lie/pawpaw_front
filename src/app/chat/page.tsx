0ad49ddaf153133afab18d91f6c1a71c37c1a5ec
[React1] 이주영 <gmltns5723@kakao.com>
2025-01-08 14:29:29 +0900

    🩹 푸터 채팅->산책메이트
    푸터 채팅->산책메이트

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -1,9 +1,8 @@
 import Footer from "@/components/Footer";
-import ChatList from "./ChatList";
 import ChatRoomPage from "./[id]/page"
 
 export const metadata = {
-  title : "채팅방 - 포포",
+  title: "채팅방 - 포포",
 };
 
 
