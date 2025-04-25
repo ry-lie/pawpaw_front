7b4e836086f8dda9f68f0ebff69e079f08f0a621
[React1] 이서빈 <tjqls0916@gmail.com>
2025-04-25 20:35:13 +0900

    fix: prevent socket init during SSR

--- src/utils/socket.ts
+++ src/utils/socket.ts
@@ -1,11 +1,19 @@
+// src/utils/socket.ts
 import { io, Socket } from "socket.io-client";
 
 const SERVER_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
 
-const socket: Socket = io(SERVER_URL, {
-  transports: ["websocket"],
+let socket: Socket;
 
-  withCredentials: true,
-});
+// 클라이언트에서만 소켓 연결
+if (typeof window !== "undefined") {
+  socket = io(SERVER_URL as string, {
+    transports: ["websocket"],
+    withCredentials: true,
+  });
+} else {
+  // 서버에서는 빈 객체로 대체 (사용되지 않도록 처리)
+  socket = {} as Socket;
+}
 
 export default socket;
